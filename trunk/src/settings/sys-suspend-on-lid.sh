1c661a113401ad9e2f84c8062c7f9eca23955003
Alex Vesev <alex.vesev@gmail.com>
2017-12-03 20:23:50 +0300

    Licence inserted.

--- trunk/src/settings/sys-suspend-on-lid.sh
+++ trunk/src/settings/sys-suspend-on-lid.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 #
 
+#
+#  Copyright 2017 Alex Vesev
+#
+#  This file is part of Script Crypt.
+#
+#  Script Crypt is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Script Crypt is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Script Crypt. If not, see <http://www.gnu.org/licenses/>.
+#
+##
+
+
 ### BEGIN INIT INFO
 # Provides:        sys-suspend-on-lid
 # Default-Start:   2 3 4 5
