098db92366394728ebcf0f40db85b2203aa735db
Alex Vesev <alex.vesev@gmail.com>
2021-10-10 19:46:40 +0300

    Bluetooth trigger script. Install action.

--- trunk/deb-build/installer.sh
+++ trunk/deb-build/installer.sh
@@ -62,6 +62,7 @@ install --owner=root --group=root --mode=755 --verbose "${DIR_ORIGIN}/logger-out
 install --owner=root --group=root --mode=755 --verbose "${DIR_ORIGIN}/script-crypt-files"               "${CHROOT}/usr/bin/script-crypt-files"
 install --owner=root --group=root --mode=755 --verbose "${DIR_ORIGIN}/fbpanel-launch"                   "${CHROOT}/usr/bin/fbpanel-launch"
 install --owner=root --group=root --mode=755 --verbose "${DIR_ORIGIN}/generate-audacity-labels"         "${CHROOT}/usr/bin/generate-audacity-labels"
+install --owner=root --group=root --mode=755 --verbose "${DIR_ORIGIN}/trigger-bt-state"                 "${CHROOT}/usr/bin/trigger-bt-state"
 
 ln -s "/etc/init.d/sys-suspend-on-lid" "${CHROOT}/etc/rc3.d/S07sys-suspend-on-lid"
 
