3888a4544dae88d4b48154429dd6cc619cd47e3e
Alex Vesev <alex.vesev@gmail.com>
2024-12-05 22:08:11 +0100

    Scanned images processing helpers.

--- trunk/deb-build/installer.sh
+++ trunk/deb-build/installer.sh
@@ -69,6 +69,8 @@ install --owner=root --group=root --mode=755 --verbose "${DIR_ORIGIN}/bt/bt-conn
 install --owner=root --group=root --mode=755 --verbose "${DIR_ORIGIN}/ap-wake-up"                       "${CHROOT}/usr/bin/ap-wake-up"
 install --owner=root --group=root --mode=755 --verbose "${DIR_ORIGIN}/refresh-known-test-nodes"         "${CHROOT}/usr/bin/refresh-known-test-nodes"
 install --owner=root --group=root --mode=755 --verbose "${DIR_ORIGIN}/net-reset"                        "${CHROOT}/usr/bin/net-reset"
+install --owner=root --group=root --mode=755 --verbose "${DIR_ORIGIN}/images-to-pdf-with-reduce"        "${CHROOT}/usr/bin/images-to-pdf-with-reduce"
+install --owner=root --group=root --mode=755 --verbose "${DIR_ORIGIN}/reduce-image"                     "${CHROOT}/usr/bin/reduce-image"
 
 ln -s "/etc/init.d/sys-suspend-on-lid" "${CHROOT}/etc/rc3.d/S07sys-suspend-on-lid"
 
